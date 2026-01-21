f5783148ac03c21771b9f59fac781d4633ea4638
SOLAMU <joeunsol030@gmail.com>
2026-01-21 10:47:20 +0900

    [프로그래머스] 대장균의 크기에 따라 분류하기 1 풀이 제출
    - 문제 ID: 299307
    - 난이도: unknown
    - 제출일: 2026. 01. 21. 10:47:20
    - 작성자: 조은솔

--- programmers/other/299307_대장균의_크기에_따라_분류하기_1.py
+++ programmers/other/299307_대장균의_크기에_따라_분류하기_1.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/299307
 # 작성자: 조은솔
-# 작성일: 2026. 01. 21. 10:47:02
+# 작성일: 2026. 01. 21. 10:47:20
 
 SELECT 
     id,
