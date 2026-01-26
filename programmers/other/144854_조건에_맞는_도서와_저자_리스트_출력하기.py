073cd0aeb9dd3e9c189a7a3df304afa4ef25d621
SOLAMU <joeunsol030@gmail.com>
2026-01-26 10:24:54 +0900

    [프로그래머스] 조건에 맞는 도서와 저자 리스트 출력하기 풀이 제출
    - 문제 ID: 144854
    - 난이도: unknown
    - 제출일: 2026. 01. 26. 10:24:54
    - 작성자: 조은솔

--- programmers/other/144854_조건에_맞는_도서와_저자_리스트_출력하기.py
+++ programmers/other/144854_조건에_맞는_도서와_저자_리스트_출력하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/144854
 # 작성자: 조은솔
-# 작성일: 2026. 01. 26. 10:23:28
+# 작성일: 2026. 01. 26. 10:24:54
 
 -- 코드를 입력하세요
 SELECT
