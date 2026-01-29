1723d963b43f8574aa69b107b6b5c196cdb0d249
SOLAMU <joeunsol030@gmail.com>
2026-01-29 09:20:13 +0900

    [프로그래머스] [PCCE 기출문제] 3번 / 수 나누기 풀이 제출
    - 문제 ID: 340205
    - 난이도: unknown
    - 제출일: 2026. 01. 29. 09:20:13
    - 작성자: 조은솔

---
+++ programmers/other/340205_[PCCE_기출문제]_3번_수_나누기.py
@@ -0,0 +1,15 @@
+# [PCCE 기출문제] 3번 / 수 나누기
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/340205
+# 작성자: 조은솔
+# 작성일: 2026. 01. 29. 09:20:13
+
+number = int(input())
+
+answer = 0
+
+while number > 0:
+    answer += number % 100
+    number //= 100
+
+print(answer)
